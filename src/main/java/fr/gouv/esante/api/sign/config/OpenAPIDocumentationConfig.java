d835c3d5dfffb64e626cb40e3528dc482441744d
mjuravliov <Maxim.JURAVLIOV.EXT@esante.gouv.fr>
2023-11-07 17:14:53 +0100

    fix pom versions, springdoc openapi partial implementation

--- src/main/java/fr/gouv/esante/api/sign/config/OpenAPIDocumentationConfig.java
+++ src/main/java/fr/gouv/esante/api/sign/config/OpenAPIDocumentationConfig.java
@@ -7,24 +7,21 @@ package fr.gouv.esante.api.sign.config;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import fr.gouv.esante.api.sign.service.impl.utils.Version;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.UriComponentsBuilder;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.Paths;
-import springfox.documentation.spring.web.paths.DefaultPathProvider;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import jakarta.servlet.ServletContext;
 import java.text.ParseException;
@@ -52,7 +49,15 @@ public class OpenAPIDocumentationConfig {
      *
      * @return the api info
      */
-    ApiInfo apiInfo() {
+    @Bean
+    public OpenAPI apiInfo() {
+        Info info = new Info();
+        info.title("ESignSante");
+        info.description("API du composant ESignSante. Ce composant dit de \"signature\" mutualise et homogénéise " +
+                "la mise en oeuvre des besoins autour de la signature électronique. Il permet aux partenaires " +
+                "de l'ANS de signer leurs documents ainsi que de vérifier la validité " +
+                "d'une signature ou d'un certificat.");
+        
         Version wsVersion = new Version(MAJOR, 0, 0, 0);
         try {
             // assign the current version of esignsante-webservices
@@ -60,13 +65,17 @@ public class OpenAPIDocumentationConfig {
         } catch (final ParseException e) {
             log.error(ExceptionUtils.getStackTrace(e));
         }
-        return new ApiInfoBuilder().title("ESignSante").description(
-                "API du composant ESignSante.  Ce composant dit de \"signature\" mutualise et homogénéise " +
-                        "la mise en oeuvre des besoins autour de la signature.  Il permet aux partenaires " +
-                        "de l'ANS de signer leurs documents ainsi que de vérifier la validité " +
-                        "d'une signature ou d'un certificat.     ")
-                .license("").licenseUrl("http://unlicense.org").termsOfServiceUrl("").version(wsVersion.getVersion())
-                .contact(new Contact("", "", "esignsante@asipsante.fr")).build();
+        info.version(wsVersion.getVersion());
+        
+        License license = new License();
+        license.setUrl("http://unlicense.org");
+        info.license(license);
+        
+        Contact contact = new Contact();
+        contact.setEmail("esignsante@asipsante.fr");
+        info.contact(contact);
+        
+        return new OpenAPI().info(info);
     }
 
     /**
@@ -76,22 +85,22 @@ public class OpenAPIDocumentationConfig {
      * @param basePath the base path
      * @return the docket
      */
+    /*
     @SuppressWarnings("unchecked")
 	@Bean
-    public <T> Docket customImplementation(@Value("${openapi.aSIPSign.base-path:/}") final String basePath) {
-        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+    public <T> GroupedOpenApi customImplementation(@Value("${openapi.esignsante.base-path:/}") final String basePath) {
+        return new GroupedOpenApi(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
                 .apis(RequestHandlerSelectors.basePackage("fr.gouv.esante.api.sign.ws.api"))
                 .paths((Predicate<String>) Predicates.not((Predicate<T>) PathSelectors.regex("/error.*"))).build()
                 .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class).apiInfo(apiInfo());
     }
-
+*/
     /**
      * The Class BasePathAwareRelativePathProvider.
      */
-    static class BasePathAwareRelativePathProvider extends DefaultPathProvider {
+    /*static class BasePathAwareRelativePathProvider extends DefaultPathProvider {
 
-        /** The base path. */
         private String basePath;
 
         /**
@@ -100,7 +109,7 @@ public class OpenAPIDocumentationConfig {
          * @param servletContext the servlet context
          * @param basePath       the base path
          */
-        public BasePathAwareRelativePathProvider(final ServletContext servletContext, final String basePath) {
+        /*public BasePathAwareRelativePathProvider(final ServletContext servletContext, final String basePath) {
             this.basePath = basePath;
         }
 
@@ -110,12 +119,12 @@ public class OpenAPIDocumentationConfig {
          * @see springfox.documentation.spring.web.paths.AbstractPathProvider#
          * getOperationPath(java.lang.String)
          */
-        @Override
+        /*@Override
         public String getOperationPath(final String operationPath) {
             final UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath("/");
             return Paths.removeAdjacentForwardSlashes(uriComponentsBuilder.path(operationPath.replaceFirst(
                     "^" + basePath, "")).build().toString());
         }
-    }
+    }*/
 
 }
