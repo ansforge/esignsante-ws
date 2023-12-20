2aa19016eb664fd7c6f5c32137a35610b4f9c20f
mjuravliov <Maxim.JURAVLIOV.EXT@esante.gouv.fr>
2023-12-20 15:14:30 +0100

    ip logging fix, elementToSign fix

--- src/main/java/fr/gouv/esante/api/sign/config/IPAddressInterceptor.java
+++ src/main/java/fr/gouv/esante/api/sign/config/IPAddressInterceptor.java
@@ -7,6 +7,7 @@ package fr.gouv.esante.api.sign.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,12 +20,37 @@ public class IPAddressInterceptor implements HandlerInterceptor {
 	 */
 	Logger log = LoggerFactory.getLogger(IPAddressInterceptor.class);
 
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+        };
  
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
- 
-        String ipAddress = request.getRemoteAddr();
-        log.info(ipAddress);
+        String ip = "";
+    	if (RequestContextHolder.getRequestAttributes() == null) {
+    		log.info("no request attributes - found ip: " +"0.0.0.0");
+        }
+
+    	for (String header: IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                ip = ipList.split(",")[0];
+                log.info("ip: " +ip);
+            }
+        }
+
+        ip = request.getRemoteAddr();
+        log.info("remote addr: "+ip);
  
         return true;
     }
